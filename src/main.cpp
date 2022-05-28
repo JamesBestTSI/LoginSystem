4cef467fac93fb81ed09fa487fa8b6519e43100f
Musical-Phoenix <musical-phoenix@hotmail.co.uk>
2022-05-28 13:53:29 +0100

    Admin Commands Added

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,11 @@
 
 CoreFunctions core = CoreFunctions();
 std::map<std::pair<std::string, std::string>, std::string> UsersData;
+std::pair<std::string, std::string> AdminAccountInfo;
+std::string LoggedInUser;
 
-std::string GetUserString(const std::string& preInputMessage){
+std::string GetUserString(const std::string &preInputMessage)
+{
     std::cout << preInputMessage;
     std::string Input = core.GetString();
     std::cout << "\n";
@@ -15,6 +18,7 @@ std::string GetUserString(const std::string& preInputMessage){
 }
 
 void AddNewUser(){
+    system("cls");
     std::string Username        = GetUserString("Please Enter A Username: ");
     std::string Password        = GetUserString("Please Enter A Password: ");
     std::string SecretMessage   = GetUserString("Please Enter A Secret Message: ");
@@ -27,30 +31,106 @@ void AddNewUser(){
     system("cls");
 }
 
-
-bool DisplaySecretMessage(const std::string& Username, const std::string& Password){
-    for (const auto &[UserDetails, Message] : UsersData){
-        if (Username == UserDetails.first && Password == UserDetails.second){
-            std::cout << Username << " your secret message is\n" << Message << "\n\n";
-            return true;
-        }
-    }
-    return false;
+bool UserIsAdmin(const std::string &Username, const std::string &Password){
+    return (Username == AdminAccountInfo.first && Password == AdminAccountInfo.second);
+}
+bool UserIsAdmin(const std::string &Username){
+    return (Username == AdminAccountInfo.first);
 }
 
-void AttemptLogin(){
+void DisplaySecretMessage(const std::string &Username){
+    for (const auto &[UserDetails, Message] : UsersData){
+        if (Username == UserDetails.first)
+        {std::cout << Username << " your secret message is\n" << Message << "\n";}
+    }
+}
+
+bool AttemptLogin(){
+    system("cls");
     std::string LoginUsername = GetUserString("Please Enter A Username: ");
     std::string LoginPassword = GetUserString("Please Enter A Password: ");
 
-    if (!DisplaySecretMessage(LoginUsername, LoginPassword))
-    {std::cout << "Username and Password does not match a previously entered Username and Password\n\n";}
+    UserIsAdmin(LoginUsername, LoginPassword) ? LoggedInUser = AdminAccountInfo.first : LoggedInUser = "";
+
+    if (LoggedInUser == ""){
+        for (const auto &[UserDetails, Message] : UsersData){
+            if (LoginUsername == UserDetails.first && LoginPassword == UserDetails.second){
+                LoggedInUser = LoginUsername;
+            }
+        }
+    }
+
+    if (LoggedInUser == ""){std::cout << "Username and Password does not match a previously entered Username and Password\n\n";}
+    return (LoggedInUser != "");
+}
+
+std::pair<std::string, std::string> FindUser(const std::string& Username){
+    std::pair<std::string, std::string> user;
+    user.first = "";
+    user.second = "";
+    for (const auto &[UserDetails, Message] : UsersData){
+        if (Username == UserDetails.first){
+            user = UserDetails;
+        }
+    }
+    return user;
+}
+
+void ListUsers(){
+    if (UsersData.size() == 0){std::cout << "There are no users available!\n";}
+    for (const auto &[UserDetails, Message] : UsersData){
+        std::cout << "Username: " << UserDetails.first << "\n";
+    }
     core.WaitForKeyPress();
     system("cls");
 }
 
+void RemoveUser(){
+    if (UsersData.size() == 0){std::cout << "There are no users available!\n";}
+    else{
+        std::cout << "To Remove A User, ";
+        std::string UserToRemove = GetUserString("Please Enter A Username: ");
+        std::pair<std::string, std::string> user = FindUser(UserToRemove);
+        if(user.first!="") {
+            UsersData.erase(user);
+            std::cout << UserToRemove << " has been removed!\n";
+        }
+        else{std::cout << UserToRemove << " does not exist!\n";}
+    }
+    core.WaitForKeyPress();
+    system("cls");
+}
+
+void AdminOptions(){
+    system("cls");
+    std::cout << "Welcome " << LoggedInUser << "\n";
+    while (true)
+    {
+        std::cout << "Options:\n1. List Users\n2. Remove User\n";
+        int Option = core.GetInt();
+        switch (Option)
+        {
+        case 1:
+        {
+            ListUsers();
+            break;
+        }
+        case 2:
+        {
+            RemoveUser();
+            break;
+        }
+        default:
+            break;
+        }
+    }
+}
+
 int main()
 {
-    UsersData.insert({std::make_pair(std::string("ADMIN"), std::string("PASSWORD")), "THIS IS THE SECRET ADMIN ACCOUNT"});
+    system("cls");
+    AdminAccountInfo = std::make_pair(std::string("ADMIN"), std::string("PASSWORD"));
+
     while (true)
     {
         std::cout << "Options:\n1. Add User\n2. Login\n";
@@ -64,7 +144,12 @@ int main()
         }
         case 2:
         {
-            AttemptLogin();
+            if (AttemptLogin()){
+                if (!UserIsAdmin(LoggedInUser)){DisplaySecretMessage(LoggedInUser);}
+                else {AdminOptions();}
+            }
+            core.WaitForKeyPress();
+            system("cls");
             break;
         }
         default:
